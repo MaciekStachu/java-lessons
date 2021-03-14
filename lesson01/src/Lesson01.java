d3ed9016d55f6325f30022df4d1997e67ab70d3d
DESKTOP-4F7KR0I\Maciek <stachowmaciej@gmail.com>
2021-03-14 21:49:24 +0100

    reading age

--- lesson01/src/Lesson01.java
+++ lesson01/src/Lesson01.java
@@ -1,5 +1,11 @@
+import java.util.Scanner;
+
 public class Lesson01 {
     public static void main(String[] args) {
-        System.out.println("Maciek to król");
+       // System.out.println("Maciek to król");
+        System.out.println("Podaj swoj wiek");
+        int wiek = new Scanner(System.in). nextInt();
+        System.out.println("Twoj wiek to: " + wiek);
+        
     }
 }

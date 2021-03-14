d79e123e5e88438782356c2da94fe393c596de6e
DESKTOP-4F7KR0I\Maciek <stachowmaciej@gmail.com>
2021-03-14 22:16:38 +0100

    asess person age

--- lesson01/src/Lesson01.java
+++ lesson01/src/Lesson01.java
@@ -2,10 +2,17 @@ import java.util.Scanner;
 
 public class Lesson01 {
     public static void main(String[] args) {
-       // System.out.println("Maciek to król");
+        // System.out.println("Maciek to król");
         System.out.println("Podaj swoj wiek");
-        int wiek = new Scanner(System.in). nextInt();
+        int wiek = new Scanner(System.in).nextInt();
         System.out.println("Twoj wiek to: " + wiek);
-        
+
+        if ((wiek > 0) && (wiek < 20)) {
+            System.out.println("Jestes mlody");
+        } else if (wiek >= 20) {
+            System.out.println("Jestes w srdnim wikeu lub stary");
+        } else {
+            System.out.println("Wprowadziles zly wiek");
+        }
     }
 }

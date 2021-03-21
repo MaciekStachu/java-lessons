c3d02cbfdb9dd7f512892f35076fc5f347c5a166
DESKTOP-4F7KR0I\Maciek <stachowmaciej@gmail.com>
2021-03-21 21:09:05 +0100

    NPSScore

--- lesson01/src/NPSScore.java
+++ lesson01/src/NPSScore.java
@@ -5,15 +5,15 @@ public class NPSScore {
     public static void main(String[] args) {
         System.out.println("Jak oceniasz nasz produkt:");
         int ocena = new Scanner(System.in).nextInt();
+
         if (ocena < 0) System.out.println("Wpisz wartość z zakresu 0-10");
         if (ocena > 10) System.out.println("Wpisz wartość z zakresu 0-10");
+
         if ((ocena >= 0) && (ocena <= 6)) {
             System.out.println("jesteś detractorem");
-        }
-        if ((ocena >= 7) && (ocena <= 8)) {
+        } else if ((ocena >= 7) && (ocena <= 8)) {
             System.out.println("jesteś nautralny");
-        }
-        if ((ocena >= 9) && (ocena <= 10)) {
+        } else if ((ocena >= 9) && (ocena <= 10)) {
             System.out.println("promujesz nasz produkt");
         }
     }

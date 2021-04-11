6d065753a498a6ee933279485f4917cf93513c86
DESKTOP-4F7KR0I\Maciek <stachowmaciej@gmail.com>
2021-04-11 21:34:18 +0200

    Tablicakwadrat

---
+++ lesson01/Homework/src/Tablicakwadrat.java
@@ -0,0 +1,19 @@
+public class Tablicakwadrat {
+    //ciag arytmetyczny
+    public static void main(String[] args) {
+        int[] tab = new int[10];
+        int len = tab.length;
+        int x = 1;
+        for (int i = 0; i < len; i ++){
+            tab[i] = x;
+            x +=1;
+        }
+        System.out.print("tablica:{");
+
+        for (int i = 0; i < len; i++){
+            System.out.print(tab[i] * tab[i]);
+            if(i < len - 1) System.out.print(",");
+        }
+        System.out.print("}");
+    }
+}
